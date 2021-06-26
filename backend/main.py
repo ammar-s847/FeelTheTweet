98c2b09aa38f433fb624753f6e9870ce4d6bb79d
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2021-06-26 19:39:56 -0400

    added api resources

--- backend/main.py
+++ backend/main.py
@@ -8,14 +8,53 @@ app = Flask(__name__)
 api = Api(app)
 
 mongo = MongoClient(MONGO_CONNECT)
-db = mongo.get_database('feel-the-tweet')
-stock_data = db.tweets
+db = mongo.get_database("feel-the-tweet")
+tweets = db.tweets
+scores = db.scores
 
-class backend(Resource):
+def find_score():
+    pass
+
+def obtain_tweets():
+    output = []
+    return output
+
+def add_new(input):
+    score = find_score(input)
+    scores.insert_one({"keyword":"input", "score name":score})
+
+def remove_record(input):
+    #tweets.delete_many({"keyword":input})
+    scores.delete_one({"keyword":input})
+
+class Backend(Resource):
     def get(self, function, f_input=None):
-        return {"status":"400", "output":"status-400"}
+        # Add new set of tweets
+        if function == "new":
+            if f_input != None or f_input != "":
+                add_new(f_input)
+                return {"status":"200"}
+            else:
+                return {"status":"400"}
+        # remove tweets
+        elif function == "remove":
+            if f_input != None or f_input != "":
+                remove_record(f_input)
+                return {"status":"200"}
+            else:
+                return {"status":"400"}
 
-api.add_resource(backend, "/<string:function>")
+class GetAll(Resource):
+    def get(self):
+        pass
+
+class GetOne(Resource):
+    def get(self, word):
+        pass
+
+api.add_resource(Backend, "/function/<string:function>/<string:f_input>")
+api.add_resource(GetAll, "/get-all")
+api.add_resource(GetOne, "/get/<string:word>")
 
 if __name__ == "__main__":
     app.run(debug=True)

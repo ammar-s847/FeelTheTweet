6492e8fe9b3e5ba95a8d5d209714cb593d5806d6
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2021-06-27 10:29:11 -0400

    fixed issues in backend

--- backend/main.py
+++ backend/main.py
@@ -9,28 +9,30 @@ from config import MONGO_CONNECT, TW_API_KEY, TW_API_SECRET, TW_ACCESS_TOKEN, TW
 app = Flask(__name__)
 api = Api(app)
 
-# PyMongo Setup
+# PyMongo Setup --------------------------------------------------------------------
 mongo = MongoClient(MONGO_CONNECT)
 db = mongo.get_database("feel-the-tweet")
 #tweets = db.tweets
 scores = db.scores
 
-# Twitter API
+# Twitter API ----------------------------------------------------------------------
 auth = tw.OAuthHandler(consumer_key=TW_API_KEY, consumer_secret=TW_API_SECRET)
 auth.set_access_token(TW_ACCESS_TOKEN, TW_TOKEN_SECRET)
 twitter_api = tw.API(auth)
 
+# Sentiment Analysis ---------------------------------------------------------------
 reader = SentimentIntensityAnalyzer()
 
 def get_sentiment_score(input):
-    return reader.polarity_scores(input)['compound'] # returns 'compound' numeric value which represents a sentiment score from -1 (negative) to 1 (positive)
+    return reader.polarity_scores(input)['compound'] 
+    # returns 'compound' numeric value which represents a sentiment score from -1 (negative) to 1 (positive)
     
 def avg_sentiment(items):
-    count=0
-    total=0
+    count = 0
+    total = 0
     for item in items:
        total += get_sentiment_score(item)
-       count+=1
+       count += 1
     return total/count
     # returns average sentiment score of a list of strings
 
@@ -42,53 +44,77 @@ def sentiment_name(score):
     elif - 0.1 <= score <= 0.1:
         return 'Neutral'
     elif 0.1 < score <= 0.55:
-        return "Positive"
-    elif 0.55 < score < 1:
-        return "Very Positive"
+        return 'Positive'
+    elif 0.55 < score <= 1:
+        return 'Very Positive'
     # returns a label describing the results of the sentiment score
 
+# CRUD Functions -------------------------------------------------------------------
 def add_new(keyword, quantity, since):
+    global twitter_api
     cursor = tw.Cursor(twitter_api.search, q=keyword, tweet_mode='extended', lang='en', since=since).items(quantity)
     tweet_list = []
+     
     for i in cursor:
         tweet_list.append(i.full_text)
+
     score = avg_sentiment(tweet_list)
     score_name = sentiment_name(score)
-    # add: check for if keyword already exists
-    scores.insert_one({"keyword":keyword, "score":score, "score-name":score_name, "quantity":quantity, "since":since})
 
-def remove_record(input):
-    #tweets.delete_many({"keyword":input})
-    scores.delete_one({"keyword":input})
+    check = scores.find_one({"keyword":keyword})
+    if check == None or len(check) == 0:
+        scores.insert_one({"keyword":keyword, "score":score, "score-name":score_name, "quantity":quantity, "since":since})
 
-class Backend(Resource):
-    def get(self, function, f_input=None):
-        # Add new set of tweets
-        if function == "new":
-            if f_input != None or f_input != "":
-                add_new(f_input)
+def remove_record(keyword):
+    #tweets.delete_many({"keyword":keyword})
+    scores.delete_one({"keyword":keyword})
+
+# Flask REST API -------------------------------------------------------------------
+class AddKeyword(Resource):
+    def get(self, keyword, since, quantity=10):
+        if keyword != None or keyword != "":
+            try:
+                add_new(keyword, quantity, since)
                 return {"status":"200"}
-            else:
-                return {"status":"400"}
-        # remove tweets
-        elif function == "remove":
-            if f_input != None or f_input != "":
-                remove_record(f_input)
+            except:
+                return {"status":"404"}
+        else:
+            return {"status":"400"}
+
+class RemoveKeyword(Resource):
+    def get(self, keyword):
+        if keyword != None or keyword != "":
+            try:
+                remove_record(keyword)
                 return {"status":"200"}
-            else:
-                return {"status":"400"}
+            except:
+                return {"status":"404"}
+        else:
+            return {"status":"400"}
 
 class GetAll(Resource):
     def get(self):
-        pass
+        try:
+            scores_db = scores.find()
+            output = []
+            for i in scores_db:
+                output.append({"keyword":i["keyword"], "score":i["score"], "score-name":i["score-name"], "quantity":i["quantity"], "since":i["since"]})
+            return {"status":"200", "data":output}
+        except:
+            return {"status":"400"}
 
 class GetOne(Resource):
-    def get(self, word):
-        pass
+    def get(self, keyword):
+        try:
+            output = scores.find_one({"keyword":keyword})
+            return {"status":"200", "data":{"keyword":output["keyword"], "score":output["score"], "score-name":output["score-name"], "quantity":output["quantity"], "since":output["since"]}}
+        except:
+            return {"status":"400"}
 
-api.add_resource(Backend, "/function/<string:function>/<string:f_input>")
-api.add_resource(GetAll, "/get-all")
-api.add_resource(GetOne, "/get/<string:word>")
+api.add_resource(AddKeyword, "/add/<string:keyword>/<string:since>/<int:quantity>")
+api.add_resource(RemoveKeyword, "/remove/<string:keyword>")
+api.add_resource(GetAll, "/all")
+api.add_resource(GetOne, "/get/<string:keyword>")
 
 if __name__ == "__main__":
     app.run(debug=True)

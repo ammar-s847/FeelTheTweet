33838573d48eb2b685609ad93758549f5b82418d
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2021-06-26 21:23:19 -0400

    implemented Twitter API and Sentiment analysis

--- backend/config.empty.py
+++ backend/config.empty.py
@@ -1,2 +1,8 @@
 # MongoDB Connection String
 MONGO_CONNECT = ""
+
+# Twitter API keys and tokens
+TW_ACCESS_TOKEN = ""
+TW_TOKEN_SECRET = ""
+TW_API_KEY = ""
+TW_API_SECRET = ""
--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,61 @@
 import requests
+import tweepy as tw
 from pymongo import MongoClient
 from flask import Flask
 from flask_restful import Api, Resource
-from config import MONGO_CONNECT
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from config import MONGO_CONNECT, TW_API_KEY, TW_API_SECRET, TW_ACCESS_TOKEN, TW_TOKEN_SECRET
 
 app = Flask(__name__)
 api = Api(app)
 
+# PyMongo Setup
 mongo = MongoClient(MONGO_CONNECT)
 db = mongo.get_database("feel-the-tweet")
-tweets = db.tweets
+#tweets = db.tweets
 scores = db.scores
 
-def find_score():
-    pass
+# Twitter API
+auth = tw.OAuthHandler(consumer_key=TW_API_KEY, consumer_secret=TW_API_SECRET)
+auth.set_access_token(TW_ACCESS_TOKEN, TW_TOKEN_SECRET)
+twitter_api = tw.API(auth)
 
-def obtain_tweets():
-    output = []
-    return output
+reader = SentimentIntensityAnalyzer()
 
-def add_new(input):
-    score = find_score(input)
-    scores.insert_one({"keyword":"input", "score-name":score})
+def get_sentiment_score(input):
+    return reader.polarity_scores(input)['compound'] # returns 'compound' numeric value which represents a sentiment score from -1 (negative) to 1 (positive)
+    
+def avg_sentiment(items):
+    count=0
+    total=0
+    for item in items:
+       total += get_sentiment_score(item)
+       count+=1
+    return total/count
+    # returns average sentiment score of a list of strings
+
+def sentiment_name(score): 
+    if -1 <= score < -0.55:
+        return 'Very Negative'
+    elif -0.55 <= score < -0.1:
+        return 'Negative'
+    elif - 0.1 <= score <= 0.1:
+        return 'Neutral'
+    elif 0.1 < score <= 0.55:
+        return "Positive"
+    elif 0.55 < score < 1:
+        return "Very Positive"
+    # returns a label describing the results of the sentiment score
+
+def add_new(keyword, quantity, since):
+    cursor = tw.Cursor(twitter_api.search, q=keyword, tweet_mode='extended', lang='en', since=since).items(quantity)
+    tweet_list = []
+    for i in cursor:
+        tweet_list.append(i.full_text)
+    score = avg_sentiment(tweet_list)
+    score_name = sentiment_name(score)
+    # add: check for if keyword already exists
+    scores.insert_one({"keyword":keyword, "score":score, "score-name":score_name, "quantity":quantity, "since":since})
 
 def remove_record(input):
     #tweets.delete_many({"keyword":input})
